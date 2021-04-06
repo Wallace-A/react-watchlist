9945eea4a65b45a6565cb5e1c447b2193ed370c8
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-04-06 16:27:09 +0900

    add movie search page

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,11 +1,27 @@
 import React, {useState} from 'react'
+import {ResultCard} from "./ResultCard";
+
 export const Add = () => {
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
   //update search text on input
   const onChange = (e) => {
     e.preventDefault();
     setQuery(e.target.value);
-  }
+    //get list of movies using api
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`).then(
+      results =>results.json())
+      .then(data => {
+        if(!data.errors) {
+          //if no errors then use data
+          setResults(data.results);
+        } else {
+          // else set results to empty
+          setResults([]);
+        }
+      });
+  };
+
   return (
     <div className="add-page">
       <div className="container">
@@ -16,6 +32,15 @@ export const Add = () => {
             value={query}
             onChange={onChange}/>
           </div>
+          {results.length > 0 && (
+            <ul className="results">
+              {results.map(movie => (
+                <li key={movie.id}>
+                  <ResultCard movie={movie}/>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
